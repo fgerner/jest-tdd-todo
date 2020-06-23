c3dbc6b6739ddb11fa33d5c4cab99341e67dd8fb
Fred <fred.gerner@icloud.com>
2020-06-23 22:30:46 +1000

    first commit

--- controllers/todo.test.js
+++
@@ -1,7 +0,0 @@
-const TodoController = require('./todoController');
-
-describe('TodoController.createTodo', () => {
-    it('should have a create todo function', () => {
-        expect(typeof TodoController.createTodo).toBe('function');
-    });
-})
