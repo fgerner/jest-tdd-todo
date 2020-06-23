43f2b77dde17e3eaa041a0427bccc3b5e4715391
Fred <fred.gerner@icloud.com>
2020-06-23 22:51:26 +1000

    added controller

---
+++ controllers/todo.test.js
@@ -0,0 +1,7 @@
+const TodoController = require('./todoController');
+
+describe('TodoController.createTodo', () => {
+    it('should have a createTodo function', () => {
+        expect(typeof TodoController.createTodo).toBe('function');
+    })
+})
