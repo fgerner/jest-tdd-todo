91dd77dc0dc202bcce7c4f188c87aa63c7b81178
Fred <fred.gerner@icloud.com>
2020-06-24 20:18:01 +1000

    refactor

--- controllers/todo.test.js
+++ controllers/todo.test.js
@@ -4,16 +4,19 @@ const httpMocks = require('node-mocks-http');
 const newTodo = require('./mock-data/new-todo.json');
 
 TodoModel.create = jest.fn();
+let req, res, next;
+
+beforeEach(() => {
+    req = httpMocks.createRequest();
+    res = httpMocks.createResponse();
+    next = null;
+})
 
 describe('TodoController.createTodo', () => {
     it('should have a createTodo function', () => {
         expect(typeof TodoController.createTodo).toBe('function');
     })
-    it('should call todo create model',  () => {
-        let req, res, next;
-        req = httpMocks.createRequest();
-        res = httpMocks.createResponse();
-        next = null;
+    it('should call todo create model', () => {
         req.body = newTodo;
         TodoController.createTodo(req, res, next);
         expect(TodoModel.create).toHaveBeenCalledWith(newTodo);
